41bcdfe87e4c4bf98696e9669f0558c80b0c7597
emigham <emigham7@gmail.com>
2023-06-27 02:20:23 +0000

    javascript

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,10 +1,8 @@
-xport default function createReportObject(employeesList) {
-	  return {
-		      allEmployees: {
-			            ...employeesList,
-			          },
-		      getNumberOfDepartments(employeesList) {
-			            return Object.keys(employeesList).length;
-			          },
-		    };
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
